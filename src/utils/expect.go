665f3711ea6cb1762031b628429fb82e3c9b549b
Dustin Graham <dustin@aja.com>
2018-03-26 16:34:43 -0700

    Remove unused batch builder.

--- src/utils/expect.go
+++ src/utils/expect.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/google/goexpect"
 	"golang.org/x/crypto/ssh"
-	"sort"
 	"time"
 )
 
@@ -27,37 +26,6 @@ func GatherExpect(batcher []expect.Batcher, timeout time.Duration, ssh *ssh.Clie
 
 	// return config data
 	return gather, err
-}
-
-// BuildBatcher builds expect.Batcher from a send and receive map
-func BuildBatcher(send map[int]string, receive map[int]string) []expect.Batcher {
-
-	// Initialize stuff
-	keys := make([]int, 0)
-	batch := make([]expect.Batcher, 0)
-
-	// Whichever is longer becomes our key list
-	if len(receive) > len(send) {
-		for k := range receive {
-			keys = append(keys, k)
-		}
-	} else {
-		for k := range send {
-			keys = append(keys, k)
-		}
-	}
-
-	// Sort, because golang
-	sort.Ints(keys)
-
-	// Set up batch, add send after every receive
-	for _, k := range keys {
-		batch = append(batch, &expect.BExp{R: receive[k]})
-		batch = append(batch, &expect.BSnd{S: send[k]})
-	}
-
-	// return
-	return batch
 }
 
 // SimpleBatcher implements a straight forward send/receive pattern.
