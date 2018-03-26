bf7c39ae6412c28312ce73e6538ad1c29316bb5a
Dustin Graham <dustin@aja.com>
2018-03-26 09:59:40 -0700

    Fix Makefile to actually fmt/lint. Cleans many spaces to tabs.

--- src/collectors/pfsense.go
+++ src/collectors/pfsense.go
@@ -1,50 +1,49 @@
 package collectors
 
 import (
-    "contra/src/utils"
-    "fmt"
-    "regexp"
-    "time"
+	"contra/src/utils"
+	"fmt"
+	"regexp"
+	"time"
 )
 
-
 func Collect() string {
-    fmt.Printf("Collect Works - pfSense\n")
+	fmt.Printf("Collect Works - pfSense\n")
 
-    // set up ssh connection - obviously not the right place for this
-    connection, err := utils.SshClient("admin", "thisshouldn'tbehere!", "192.168.1.1:22")
+	// set up ssh connection - obviously not the right place for this
+	connection, err := utils.SshClient("admin", "thisshouldn'tbehere!", "192.168.1.1:22")
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    // Output we expect to receive
-    receive := map[int]string {
-        1 : "option:", // 1 : should always match the initial connection string
-        2 : ".*root",
-        3 : "</pfsense>",
-    }
+	// Output we expect to receive
+	receive := map[int]string{
+		1: "option:", // 1 : should always match the initial connection string
+		2: ".*root",
+		3: "</pfsense>",
+	}
 
-    // Commands we will send in response to output above
-    send := map[int]string {
-        1 : "8\n",
-        2 : "cat /conf/config.xml\n",
-    }
+	// Commands we will send in response to output above
+	send := map[int]string{
+		1: "8\n",
+		2: "cat /conf/config.xml\n",
+	}
 
-    // Build batcher
-    batch := utils.BuildBatcher(send, receive)
+	// Build batcher
+	batch := utils.BuildBatcher(send, receive)
 
-    // call GatherExpect to collect the configs
-    result, err := utils.GatherExpect(batch, time.Second*10, connection)
-    if err != nil {
-        panic(err)
-    }
+	// call GatherExpect to collect the configs
+	result, err := utils.GatherExpect(batch, time.Second*10, connection)
+	if err != nil {
+		panic(err)
+	}
 
-    // Strip shell commands, grab only the xml file
-    config := regexp.MustCompile(`<\?xml version[\s\S]*?<\/pfsense>`)
+	// Strip shell commands, grab only the xml file
+	config := regexp.MustCompile(`<\?xml version[\s\S]*?<\/pfsense>`)
 
-    match := config.FindStringSubmatch(result[2].Output)
+	match := config.FindStringSubmatch(result[2].Output)
 
-    utils.WriteFile(match[0], "pfsense.txt")
-    return match[0]
+	utils.WriteFile(match[0], "pfsense.txt")
+	return match[0]
 }
