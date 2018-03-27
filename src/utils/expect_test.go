4657b65f923551ee52418862bd9a3ababd783f56
Dustin Graham <dustin@aja.com>
2018-03-27 10:32:10 -0700

    Variable batcher functionality, with pfsense implementation.

--- src/utils/expect_test.go
+++ src/utils/expect_test.go
@@ -8,7 +8,7 @@ import (
 
 // An equal number of send/receive is easier to program. Test this first.
 // We don't need to check the error return value since if there is an error, the batcher will be wrong.
-func TestSimpleBatcher_EqualSndRcv(t *testing.T) {
+func TestSimpleBatcherEqualSndRcv(t *testing.T) {
 	batcher, _ := SimpleBatcher([][]string{
 		{"a", "b"},
 		{"c", "d"},
@@ -27,7 +27,7 @@ func TestSimpleBatcher_EqualSndRcv(t *testing.T) {
 }
 
 // Having an uneven number is a little more tricky.
-func TestSimpleBatcher_NormalSndRcv(t *testing.T) {
+func TestSimpleBatcherNormalSndRcv(t *testing.T) {
 	batcher, _ := SimpleBatcher([][]string{
 		{"a", "b"},
 		{"c", "d"},
@@ -49,7 +49,7 @@ func TestSimpleBatcher_NormalSndRcv(t *testing.T) {
 
 // Having an uneven number is a little more tricky.
 // We don't need to check the batcher, since we only need to know if we have an error.
-func TestSimpleBatcher_BadInput(t *testing.T) {
+func TestSimpleBatcherBadInput(t *testing.T) {
 	_, err := SimpleBatcher([][]string{
 		{"a", "b", "wrong"},
 		{"c", "d"},
@@ -60,3 +60,38 @@ func TestSimpleBatcher_BadInput(t *testing.T) {
 		t.Fatalf("Expected an error, but did not receive an error.")
 	}
 }
+
+// I can't figure out a way to make DeepEqual compare the two Batcher slices equally.
+// In the below test, sameA and sameB are exactly the same, yet they don't equal because
+// of something to do with the pointers being used. Commenting out for now.
+// Perhaps we could loop through the batcher and check all the cases.
+//func TestVariableBatcherBasic(t *testing.T) {
+//	//batcher, _ := VariableBatcher([][]string{
+//	//	{"option:", "8\n"},
+//	//	{".*root", "cat /conf/config.xml\n"},
+//	//	{"$", "cat /conf/config.xml\n"},
+//	//	{"</pfsense>"},
+//	//})
+//
+//	sameA := []expect.Batcher{
+//		&expect.BCas{C: []expect.Caser{
+//			&expect.BCase{R: `option:`, S: "8\n", Rt: 1, T: expect.Continue(expect.NewStatus(7, "something failed"))},
+//			&expect.BCase{R: `.*root`, S: "cat /conf/config.xml\n", Rt: 1, T: expect.Continue(expect.NewStatus(7, "something failed"))},
+//			&expect.BCase{R: `$`, S: "cat /conf/config.xml\n", Rt: 1, T: expect.Continue(expect.NewStatus(7, "something failed"))},
+//			&expect.BCase{R: `</pfsense>`, T: expect.OK()},
+//		}},
+//	}
+//
+//	sameB := []expect.Batcher{
+//		&expect.BCas{C: []expect.Caser{
+//			&expect.BCase{R: `option:`, S: "8\n", Rt: 1, T: expect.Continue(expect.NewStatus(7, "something failed"))},
+//			&expect.BCase{R: `.*root`, S: "cat /conf/config.xml\n", Rt: 1, T: expect.Continue(expect.NewStatus(7, "something failed"))},
+//			&expect.BCase{R: `$`, S: "cat /conf/config.xml\n", Rt: 1, T: expect.Continue(expect.NewStatus(7, "something failed"))},
+//			&expect.BCase{R: `</pfsense>`, T: expect.OK()},
+//		}},
+//	}
+//
+//	if !reflect.DeepEqual(sameA, sameB) {
+//		t.Fatalf("VariableBatcher did not do what we expected.\nFound: %s\nExpect:%s", sameA, sameB)
+//	}
+//}
