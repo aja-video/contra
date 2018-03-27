3cdf49f02648263bf23e6b5701a4b49acf116579
Sigi Turner <sigit@aja.com>
2018-03-27 11:47:20 -0700

    Directory seperator missed when updating to config workspace

--- src/utils/writefile.go
+++ src/utils/writefile.go
@@ -10,7 +10,7 @@ import (
 func WriteFile(c configuration.Config, config string, name string) error {
 	fmt.Println(c.Workspace)
 	// Create file inside workspace folder
-	f, err := os.Create(c.Workspace + name)
+	f, err := os.Create(c.Workspace + "/" + name)
 	if err != nil {
 		panic(err)
 	}
