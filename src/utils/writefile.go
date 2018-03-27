71d4f546c6320e53c8fc0d3ca8b1f2f85e35d4a4
Sigi Turner <sigit@aja.com>
2018-03-27 13:54:52 -0700

    Handle creation of workspace directory if it doesn't exist.  Resolves #1

--- src/utils/writefile.go
+++ src/utils/writefile.go
@@ -2,22 +2,38 @@ package utils
 
 import (
 	"contra/src/configuration"
-	"fmt"
+	"log"
 	"os"
 )
 
 // WriteFile saves output to a file
 func WriteFile(c configuration.Config, config string, name string) error {
-	fmt.Println(c.Workspace)
 	// Create file inside workspace folder
-	f, err := os.Create(c.Workspace + "/" + name)
-	if err != nil {
-		panic(err)
+	err := wsDir(c)
+	if err == nil {
+		f, err := os.Create(c.Workspace + "/" + name)
+		if err != nil {
+			log.Printf("Unable to create config file %s\n", name)
+			return err
+		}
+		defer f.Close()
+		// write config data
+		f.WriteString(config)
+		f.Close()
+		f.Sync()
 	}
-	defer f.Close()
-	// write config data
-	f.WriteString(config)
-	f.Close()
+	return err
+}
+
+// wsDir checks for or creates the workspace directory
+func wsDir(c configuration.Config) error {
+	// Check if the workspace is a directory
+	if ws, err := os.Stat(c.Workspace); err == nil && ws.IsDir() {
+		return nil
+	}
+
+	// Create directory if it isn't there
+	err := os.Mkdir(c.Workspace, os.ModePerm)
 
 	return err
 }
