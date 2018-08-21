f6f85dcf2cfd7d11fb2fb2c3fc040a7b5cfd65fb
Sigi Turner <sigit@aja.com>
2018-08-21 16:38:34 -0700

    Fix missing user after upgrade

--- files/rpm/after-upgrade.sh
+++ files/rpm/after-upgrade.sh
@@ -1,1 +1,2 @@
 diff /etc/contra.conf.dist /etc/contra.conf.rpmsave > /dev/null 2>&1 || cp /etc/contra.conf.rpmsave /etc/contra.conf
+getent passwd contra > /dev/null 2>&1 || useradd -r -d /opt/contra -s /sbin/nologin contra
