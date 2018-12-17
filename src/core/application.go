fa59cabda5dd1897b500966f10464febf379b692
Dustin Graham <dustin@aja.com>
2018-12-17 09:52:14 -0800

    Bump version v1.0.51

--- src/core/application.go
+++ src/core/application.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const version = "1.0.50"
+const version = "1.0.51"
 
 // Application holds global application data and functions for kicking off execution.
 type Application struct {
