27c1c8df85ce25a2b28053b048f5137ef798b77c
Dustin Graham <dustin@aja.com>
2018-12-16 19:53:48 -0800

    Bump master version to 1.0.50

--- src/core/application.go
+++ src/core/application.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const version = "1.0.2"
+const version = "1.0.50"
 
 // Application holds global application data and functions for kicking off execution.
 type Application struct {
