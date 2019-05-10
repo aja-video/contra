ba412882377e15f0ead06f2d7c2242229bc1f227
Sigi Turner <sigit@aja.com>
2019-05-10 15:20:26 -0700

    add ubnt EdgeSwitch support

--- src/collectors/collector_registry.go
+++ src/collectors/collector_registry.go
@@ -4,11 +4,12 @@ import "github.com/aja-video/contra/src/devices"
 
 // Mandatory that new collector definitions be added to this array.
 var deviceMap = map[string]interface{}{
-	"arista":    devices.DeviceArista{},
-	"cisco_csb": devices.DeviceCiscoCsb{},
-	"comware":   devices.DeviceComware{},
-	"mikrotik":  devices.DeviceMikrotik{},
-	"pfsense":   devices.DevicePfsense{},
-	"procurve":  devices.DeviceProcurve{},
-	"vyatta":    devices.DeviceVyatta{},
+	"arista":     devices.DeviceArista{},
+	"cisco_csb":  devices.DeviceCiscoCsb{},
+	"comware":    devices.DeviceComware{},
+	"edgeswitch": devices.DeviceEdgeSwitch{},
+	"mikrotik":   devices.DeviceMikrotik{},
+	"pfsense":    devices.DevicePfsense{},
+	"procurve":   devices.DeviceProcurve{},
+	"vyatta":     devices.DeviceVyatta{},
 }
